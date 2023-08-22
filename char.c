f4e0b852c61ca57cacff6773ad5f5783aa791880
MehdiFakes <mehdifakes@gmail.com>
2023-08-22 13:48:00 -0700

    alx Task

---
+++ char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * format_c - prints a char.
+ * @arguments: arg.
+ * Return: 1.
+ */
+int format_c(va_list arguments);
+{
+	char s;
+
+	s = format.c(val, int);
+	_putchar(s);
+	return (1);
+}
