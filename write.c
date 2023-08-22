121aa2424a7b98d18547a17971c40460de53c89b
Muhadev <muhammedfayemi@gmail.com>
2023-08-22 12:54:36 -0700

    task is added

--- write.c
+++ write.c
@@ -7,7 +7,18 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _putch(char ch)
 {
-	return (write(1, &c, 1));
+static int p;
+static char buff[1024];
+if (ch == -2 || p >= 1024)
+{
+	write(1, &buff, p);
+	p = 0;
+}
+if (ch != -1)
+{
+	buff[p] = ch;
+	p++;
+return (1);
 }
